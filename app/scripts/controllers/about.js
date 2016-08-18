7d9f3dd4272bef30f34fa912bc3fdf78b78d2e90
justice <justice@tangentsolutions.co.za>
2016-08-18 08:13:59 +0200

    Updated json file, and set in unit tests

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutorialJustice02App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTutorialJustice02App
+ */
+angular.module('angularTutorialJustice02App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTutorialJustice02App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTutorialJustice02App
+ */
+angular.module('angularTutorialJustice02App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
